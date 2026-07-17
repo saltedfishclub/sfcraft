e9901af08e9236c0949905ba61006480cf0a8b90
iceBear67 <icebear67@sfclub.cc>
2026-07-17 19:59:43 +0800

    fix: disallow zombie picking up egg, rotten flesh, string, bone, arrow and wheat seeds

---
+++ src/main/java/io/ib67/sfcraft/mixin/common/ZombieMixin.java
@@ -0,0 +1,24 @@
+package io.ib67.sfcraft.mixin.common;
+
+import net.minecraft.world.entity.monster.zombie.Zombie;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Zombie.class)
+public abstract class ZombieMixin {
+    @Inject(method = "canHoldItem", at = @At("HEAD"), cancellable = true)
+    public void canHoldItem(final ItemStack itemStack, final CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(!(itemStack.is(Items.EGG)
+                || itemStack.is(Items.ROTTEN_FLESH)
+                || itemStack.is(Items.STRING))
+                || itemStack.is(Items.BONE)
+                || itemStack.is(Items.ARROW)
+                || itemStack.is(Items.WHEAT_SEEDS)
+        );
+    }
+}
